0c0e8db188690fa6c0431e3d1956d7e236a5d04e
fridaSea <maria.kluge.codes@gmail.com>
2025-02-06 15:21:16 +0100

    Set up "add a document" from firestore - messages written in the form are send to firestore

--- src/pages/Forum/Forum.tsx
+++ src/pages/Forum/Forum.tsx
@@ -1,10 +1,8 @@
 import { Box, Button, Card, CardActions, CardContent, CardMedia, Stack, TextField, Typography } from "@mui/material"
-import { Form } from "react-router"
-import SendIcon from '@mui/icons-material/Send';
-import { Email } from "@mui/icons-material";
-import { collection, getDocs, Timestamp } from "firebase/firestore";
+import { addDoc, collection, getDocs, Timestamp } from "firebase/firestore";
 import { db } from "../../configuration/firebaseConfig";
-import { useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../components/Context/AuthContext";
 
 type MessageType = {
     author: string;
@@ -14,21 +12,21 @@ type MessageType = {
 }
 
 function Forum() {
+    const{user} = useContext(AuthContext)
 
     const [messages, setMessages] = useState<MessageType[] | null >(null)
-
-
+    const [messageText, setMessageText] = useState<string>("");
 
     const getMessages = async () => {
         const querySnapshot = await getDocs(collection(db, "forum"));
-        const messagesArray:MessageType[] = []
+        const messagesArray:MessageType[] = [];
         querySnapshot.forEach((doc) => {
             console.log('doc:>>', doc)
           console.log(`${doc.id} => ${doc.data().text}`);
           const message: MessageType = {
             text: doc.data().text,
             date: doc.data().date,
-            athor: doc.data().author,
+            author: doc.data().author,
             id: doc.id
           }
           
@@ -44,60 +42,100 @@ function Forum() {
       return formatedDate;
     }
 
+    //RAUL:  e: ChangeEvent<HTMLTextAreaElement>
+    // Write the message
+    const handleMessageTextChange = (e:ChangeEvent<HTMLInputElement>) => {
+      console.log('e.target.value :>>' , e.target.value);
+      setMessageText(e.target.value);
+    }
+
+    // Submit the message 
+    const handleMessageSubmit = async (e:FormEvent<HTMLFormElement>) => {
+        // the default behaviour of a form is always to refresh the page. O prevent that with the following: 
+        e.preventDefault()
+        // if you try to send a message and you are not logged in you will get a warning here - TO DO: BUILD AN APPROPIATE ERROR MESSAGE TO SHOW TO THE USER 
+        // this is just double checking becuase it is a protected route and shouldn`t be accessabile in the first place 
+        if(!user) {
+            alert("You need to login first.")
+            return 
+        }
+        const newMessage = {
+            text: messageText,
+            date: new Date(),
+            author: user.email
+        };
+      // Add a new document with a generated id.
+        const docRef = await addDoc(collection(db, "forum"),newMessage);
+        if (!docRef) {
+            throw new Error ("Something happend!")
+            // I don`t need to return, becuase if you throw an error you don`t need to put it because it stops there 
+        }   
+        if(docRef){
+            console.log("Document written with ID: ", docRef.id);
+        }
+    
+    }
+
     useEffect(() => {
       getMessages()
     }, [])
     
-
 
   return (
     <div>
       <h1>
         This is your Place to connect - THE FORUM
       </h1>
+      <div>
       <Stack gap={3} className="align-items-center">
       
-        {messages && messages.map( (message) => {
-          return (
-            <Card sx={{ maxWidth: 345 }} key={message.id}>
-                <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {message.author}
-                </Typography>
-                <Typography gutterBottom variant="p" component="div">
-                    {formatDate(message.date.seconds)}
-                </Typography>
-                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    {message.text}
-                </Typography>
-                </CardContent>
-            </Card>
-          )
-        })}
+      {messages && messages.map( (message) => {
+        return (
+          <Card sx={{ maxWidth: 345 }} key={message.id}>
+              <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                  {message.author}
+              </Typography>
+              <Typography gutterBottom variant="p" component="div">
+                  {formatDate(message.date.seconds)}
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  {message.text}
+              </Typography>
+              </CardContent>
+          </Card>
+        )
+      })}
 
-        <Box
-        component="form"
-        sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
-        noValidate
-        autoComplete="off"
-        >
-        <div>
-            <TextField
-            id="outlined-multiline-flexible"
-            label="Your Text"
-            multiline
-            maxRows={4}
-            variant="outlined"
-            />
-            
-        </div>
-        </Box>
-        <Button type="submit" variant="outlined">
-        Send
-        </Button>
-      </Stack>
+      
+      <Box
+      component="form" onSubmit={handleMessageSubmit}
+      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+      noValidate
+      autoComplete="off"
+      
+      >
+      <div>
+          <TextField
+          id="outlined-multiline-flexible"
+          label="Your Text"
+          multiline
+          maxRows={4}
+          variant="outlined"
+          onChange={handleMessageTextChange}
+          />
+          
+      </div>
+      <Button type="submit" variant="outlined" >
+      Send
+      </Button>
+      </Box>
+     
+    </Stack>
+
+      </div>
     </div>
   )
 }
 
-export default Forum
+export default Forum;
