e2a2939c1a6a6087b2e7c3b8befe011a7c53bc8c
fridaSea <maria.kluge.codes@gmail.com>
2025-02-08 19:54:42 +0100

    Set up the welcome/splash page

--- src/pages/Splash/Splash.tsx
+++ src/pages/Splash/Splash.tsx
@@ -2,24 +2,23 @@ import './Splash.css'
 import '../../index.css'
 import { Button, Container } from '@mui/material'
 import Logo from '../../assets/Logo_round.png'
+import { Link } from 'react-router'
 // import Logo from '../../assets/Logo.png'
 
 
 function Splash() {
   return (
-    <Container>
-    <div>
-      {/* <h2 className='Logo'>This is my Logo</h2> */}
-      {/* <img src={Logo} alt="Logo Crazy Reads"/> */}
-      <img src={Logo} alt="Logo Crazy Reads"/>
-    </div>
-
-    <div>
-        <button className='button'>Welcome</button>
-    </div>
+    <Container className='splash'>
+      <div className='welcome'>
+        <h1>Happy Welcome to</h1>
+        <img src={Logo} alt="Logo Crazy Reads"/>
+        <h1>Cozy Reads</h1>
+      </div>
+      
+      <h2>Your place to relaxe and enjoy a good book</h2>
+      <button className='button'><Link to="/">Come on in ...</Link></button>
 
     </Container>
-  
 
   )
 }
